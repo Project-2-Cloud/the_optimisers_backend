228a1d5a9a694006e285ba268f7e6d50c10cdce2
milan.brouns <milan.brouns@student.ucll.be>
2021-04-01 11:35:11 +0200

    Main Controller start

--- src/main/java/theoptimisers/citygardeners/Controlers/MainController.java
+++ src/main/java/theoptimisers/citygardeners/Controlers/MainController.java
@@ -4,17 +4,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import theoptimisers.citygardeners.Service.ProductService;
-import theoptimisers.citygardeners.Service.UserService;
+import theoptimisers.citygardeners.Service.*;
 
 @Controller
 public class MainController {
 
     @Autowired
     private ProductService productService;
-
     @Autowired
     private UserService userService;
+    @Autowired
+    private ContactPointService contactPointService;
+    @Autowired
+    private ForSaleService forSaleService;
+    @Autowired
+    private SaleService saleService;
+    @Autowired
+    private UserContactPointService userContactPointService;
+    @Autowired
+    private UserWorkshopService userWorkshopService;
+    @Autowired
+    private WorkshopService workshopService;
+
 
     @GetMapping("/index")
     public String index(Model model) {
