fc96018f40cef9c00d72555640b6c3bbfd2b22e1
JoranVanGrunderbeek <55390137+JoranVanGrunderbeek@users.noreply.github.com>
2021-04-01 12:10:57 +0200

    jar file added

--- src/test/java/theoptimisers/citygardeners/CityGardenersApplicationTests.java
+++ src/test/java/theoptimisers/citygardeners/CityGardenersApplicationTests.java
@@ -1,13 +1,13 @@
-package theoptimisers.citygardeners;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class CityGardenersApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package theoptimisers.citygardeners;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class CityGardenersApplicationTests {
+////
+////	@Test
+////	void contextLoads() {
+////	}
+//
+//}
